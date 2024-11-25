69569a207745605146a63d7830b4101e5180c98f
huliua <1148247580@qq.com>
2024-11-25 14:05:24 +0800

    Initial commit

---
+++ src/layout/BaseLayout/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+
+defineComponent({
+  name: "BaseLayout",
+});
+</script>
+
+<template>
+  <div id="base-layout"></div>
+</template>
+
+<style scoped></style>
